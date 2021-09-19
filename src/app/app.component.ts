30f80ce5e38e75a4758a432681ed25c11f52a29d
Kumaresan Muthaliar <k.muthaliar@wolterskluwer.com>
2021-09-19 13:11:12 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent {
   onSubmit(): void {
     const ryhmeLen = this.ryhme.split(" ").length;
     this.safePosition = this.findSafePosition(this.servants, ryhmeLen);
-    this.servantsArr = Array.from({length: this.servants}, (v, i) => i);
+    this.servantsArr = Array.from({ length: this.servants }, (v, i) => i);
   }
 
   findSafePosition(servants: number, ryhmeLen: number): number {
@@ -29,7 +29,13 @@ export class AppComponent {
         servantArr.push(servantArr.shift());
       dragOrder.push(servantArr.shift());
     }
-    // document.getElementById("circle")
+    setTimeout(() => {
+      let elem = document.getElementById("circle" + (servantArr[0] - 1));
+      if (elem) {
+        elem.getElementsByTagName("img")[0].style.border = "solid 5px #0bba11";
+        elem.getElementsByTagName("img")[0].style.filter = "none";
+      }
+    }, 1000);
     return servantArr[0];
   }
 }
