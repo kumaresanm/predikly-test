781fc733985ee7d2722db61d338e6e608e96c107
Kumaresan Muthaliar <k.muthaliar@wolterskluwer.com>
2021-09-19 12:20:53 +0530

    Intial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'safe-position';
+  title = "safe-position";
+  servants: number;
+  ryhme = "";
+  safePosition: number;
+  servantsArr = [];
+
+  onSubmit(): void {
+    const ryhmeLen = this.ryhme.split(" ").length;
+    this.safePosition = this.findSafePosition(this.servants, ryhmeLen);
+    this.servantsArr = Array.from({length: this.servants}, (v, i) => i);
+  }
+
+  findSafePosition(servants: number, ryhmeLen: number): number {
+    let servantArr = [];
+    for (let i = 1; i <= servants; i++) servantArr.push(i);
+
+    let dragOrder = [];
+
+    while (servantArr.length !== 1) {
+      for (let skip = 1; skip < ryhmeLen; skip++)
+        servantArr.push(servantArr.shift());
+      dragOrder.push(servantArr.shift());
+    }
+    // document.getElementById("circle")
+    return servantArr[0];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule
+} from "@angular/material";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
